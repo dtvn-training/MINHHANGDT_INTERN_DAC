1d29c3df1be8c4ff70e4147390d611dec676e408
mhjame <doanthiminhhangit@gmail.com>
2024-11-06 10:43:30 +0700

    #14 fill in class AndroidDataCollector

--- assignment/phase1/crawl_data.py
+++ assignment/phase1/crawl_data.py
@@ -71,8 +71,41 @@ class IosApp(AppData):
 
 class AndroidDataCollector:
     """Android data collector Class"""
+
     def __init__(self):
         self.android_apps = []
+
+    def collect_android_data(self, app_ids):
+        """
+        Crawl app data from Google Play Store base on app_ids
+        """
+        for app_id in app_ids:
+            try:
+                # crawl app through app_id
+                result = app(
+                    app_id,
+                    lang = 'en',
+                    country = 'vi'
+                )
+
+                #create instance of AndroidApp from crawl data
+                android_app = AndroidApp(
+                    app_id=app_id,
+                    app_name=result.get("title", "Unknown"),
+                    category=result.get("genre", "Unknown"),
+                    price = float(result.get("price", "0")),
+                    provider = result.get("developer", "Unknown"),
+                    description=result.get("description", "Unknown"),
+                    developer_email= result.get("developerEmail", "Unknown"),
+                )
+
+                self.android_apps.append(android_app)
+
+            except Exception as e:
+                print(f"Failed to crawl app {app_id}: {e}")
+
+    def get_collected_android_apps(self):
+        return self.android_apps
 
 class IosDataCollector:
     """Ios data collector Class"""

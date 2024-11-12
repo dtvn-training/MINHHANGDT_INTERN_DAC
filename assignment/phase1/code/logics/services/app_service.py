f0fa001b3c4e275bc16e84a90f4fdd6936e4efd8
mhjame <doanthiminhhangit@gmail.com>
2024-11-12 14:24:58 +0700

    #14 fill in code/logics/services which contain app_service, android_service, ios_service which are use for get data from database and insert data into database

---
+++ assignment/phase1/code/logics/services/app_service.py
@@ -0,0 +1,27 @@
+# app/services/app_service.py
+from ..database import SessionLocal
+from ..models.android_app import AndroidApp
+from ..models.ios_app import IosApp
+
+class AppService:
+    def __init__(self):
+        self.session = SessionLocal()
+
+    def insert_app(self, app_obj):
+        try:
+            self.session.add(app_obj)
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            print(f"Error inserting app: {e}")
+        finally:
+            self.session.close()
+
+    def get_app_by_id(self, app_id, op_sys):
+        try:
+            if op_sys == 'android':
+                return self.session.query(AndroidApp).filter(AndroidApp.app_id == app_id).first()
+            else:
+                return self.session.query(IosApp).filter(IosApp.app_id == app_id).first()
+        finally:
+            self.session.close()

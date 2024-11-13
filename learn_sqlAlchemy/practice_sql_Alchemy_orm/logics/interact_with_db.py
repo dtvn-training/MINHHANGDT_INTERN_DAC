4d39613b6d07fae6edc6225752548943d43bc34f
mhjame <doanthiminhhangit@gmail.com>
2024-11-13 16:06:51 +0700

    #1 delete non related file

--- learn_sqlAlchemy/practice_sql_Alchemy_orm/logics/interact_with_db.py
+++
@@ -1,9 +0,0 @@
-from logics.models.actor import Actor
-
-def add_actor(session, name, fullname, body_count):
-    new_actor = Actor(name = name, fullname = fullname, body_count=body_count)
-    session.add(new_actor)
-    session.commit()
-
-def get_all_actors(session):
-    return session.query(Actor).all()
